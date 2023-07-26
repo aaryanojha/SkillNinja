694584c5da3216278c865b5ca5ebd98941c949f9
pranavmalwatkar <130284009+pranavmalwatkar@users.noreply.github.com>
2023-07-26 02:03:01 -0700

    event change

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+    
+
   }
   
-  function closeForm() {
+  function closeForm(event) {
+    event.preventDefault();
+
     document.getElementById("myForm").style.display = "none";
   }
